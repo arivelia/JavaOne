9ec5f117e15f1fc3b4b38bc65936c90f388af672
arivelia <zornitsa.ilinova@gmail.com>
2017-04-24 11:58:33 +0300

    Commit 2

--- Hellojava.java
+++ Hellojava.java
@@ -1,5 +1,4 @@
-public class MyFirstJavaProgram {
-   
+public class MyFirstJavaProgram {   
    public static void main(String []args) {
       System.out.println("Hello World"); // prints Hello World
    }
